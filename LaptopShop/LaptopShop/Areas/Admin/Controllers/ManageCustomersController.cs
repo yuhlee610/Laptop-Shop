0b84a84f82b61e951edff743d530d095f6e9cbac
DucHuy <letranduchuy41@gmail.com>
2020-11-27 16:43:05 +0700

    hiển thị danh sách customer, phân trang, tìm kiếm cập nhật file csdl

---
+++ LaptopShop/LaptopShop/Areas/Admin/Controllers/ManageCustomersController.cs
@@ -0,0 +1,20 @@
+﻿using LaptopShop.ModelController;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaptopShop.Areas.Admin.Controllers
+{
+    public class ManageCustomersController : Controller
+    {
+        // GET: Admin/ManageCustomers
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 1)
+        {
+            var model = CustomerController.ListAllPaging(searchString, page, pageSize);
+            ViewBag.Searching = searchString;
+            return View(model);
+        }
+    }
+}
---
+++ LaptopShop/LaptopShop/ModelController/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using LaptopShop.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LaptopShop.ModelController
+{
+    public class CustomerController
+    {
+        public static IEnumerable<Customer> ListAllPaging(string searchString, int page, int pageSize)
+        {
+            using (var _context=new DBLaptopEntities())
+            {
+                IQueryable<Customer> model = _context.Customers;
+                if (!string.IsNullOrEmpty(searchString))
+                {
+                    model = model.Where(x => x.accountName.Contains(searchString)
+                        || x.accountName.Contains(searchString));
+                }
+                return model.OrderByDescending(x => x.accountName).ToPagedList(page, pageSize);
+            }
+            
+        }
+    }
+}
